d327f5aba4c1961328bd1b21aeeff305e15e1c9c
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2025-12-20 23:27:28 +0100

    add middleware to routes

--- back/routes/authRoutes.js
+++ back/routes/authRoutes.js
@@ -5,9 +5,14 @@ const {
   register,
   verifyOtp
 } = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/login", login);
 router.post("/verify", verifyOtp);
 router.post("/register", register); 
+
+router.get("/dashboard", authMiddleware, (req, res) => {
+  res.json({ message: "Welcome to dashboard" });
+});
 
 module.exports = router;
