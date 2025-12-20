8659a49bcab740a925242e7e9f59b3709617a5c8
Talal Ait-Mouh <148981557+Talal-Aitmouh@users.noreply.github.com>
2025-12-20 23:29:42 +0100

    api

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: "http://localhost:3000/api/auth",
 });
 
+api.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
 export default api;
